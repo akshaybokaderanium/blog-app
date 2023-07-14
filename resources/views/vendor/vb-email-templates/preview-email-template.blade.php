05565fd0b857a2f56a8547d98430c854d2315893
Akshay Bokade <akshay.bokade@ranium.in>
2023-07-14 11:22:18 +0530

    removed email template plugin files

--- resources/views/vendor/vb-email-templates/preview-email-template.blade.php
+++
@@ -1,7 +0,0 @@
-@include('vb-email-templates::email.header')
-
-@include('vb-email-templates::email.hero_title')
-
-@include('vb-email-templates::email.content')
-
-@include('vb-email-templates::email.footer')
